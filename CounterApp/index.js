0a0d6cf1181f9f3f53b3427604b087df68e59647
Suryamani Patra <137888512+suryamanipatra@users.noreply.github.com>
2024-02-02 20:27:41 +0530

    Add files via upload

---
+++ CounterApp/index.js
@@ -0,0 +1,11 @@
+const countValue = document.querySelector('#count');
+const increment = ()=>{
+    let value = parseInt(countValue.innerText);
+    value = value + 1;
+    countValue.innerText = value;
+};
+const decrement = () =>{
+    let value = parseInt(countValue.innerText);
+    value = value - 1;
+    countValue.innerText = value;
+};
